fe27f8dce7ee00f79a433e254b37a9a4c9d5a54d
abellmanuell <mannydev02@gmail.com>
2025-09-08 22:33:37 +0100

    Add something to detail page

--- lib/product_details_page.dart
+++ lib/product_details_page.dart
@@ -9,8 +9,9 @@ class ProductDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back_ios)),
+        // leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back_ios)),
         title: Text("Details page"),
+        centerTitle: true,
       ),
 
       body: Column(
@@ -25,6 +26,39 @@ class ProductDetailsPage extends StatelessWidget {
             child: Image.asset(product["imageUrl"] as String),
           ),
           const Spacer(flex: 2),
+
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(245, 247, 249, 1),
+              borderRadius: BorderRadius.circular(40),
+            ),
+
+            child: Column(
+              children: [
+                Text(
+                  "\$${product['price']}",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: (product["sizes"] as List<int>).length,
+                    itemBuilder: (context, index) {
+                      final size = (product["sizes"] as List<int>)[index];
+
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Chip(label: Text(size.toString())),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
