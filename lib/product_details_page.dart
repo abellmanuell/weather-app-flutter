e92d980c8a9918aaba6b82afc6e1b579e3a9883e
abellmanuell <mannydev02@gmail.com>
2025-09-09 16:33:13 +0100

    Build product list and cart_page

--- lib/product_details_page.dart
+++ lib/product_details_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-class ProductDetailsPage extends StatelessWidget {
+class ProductDetailsPage extends StatefulWidget {
   final Map<String, Object> product;
 
   const ProductDetailsPage({super.key, required this.product});
+
+  @override
+  State<ProductDetailsPage> createState() => _ProductDetailsPageState();
+}
+
+class _ProductDetailsPageState extends State<ProductDetailsPage> {
+  int selectedSize = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +24,13 @@ class ProductDetailsPage extends StatelessWidget {
       body: Column(
         children: [
           Text(
-            product["title"] as String,
+            widget.product["title"] as String,
             style: Theme.of(context).textTheme.titleLarge,
           ),
           const Spacer(),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Image.asset(product["imageUrl"] as String),
+            child: Image.asset(widget.product["imageUrl"] as String),
           ),
           const Spacer(flex: 2),
 
@@ -35,9 +42,10 @@ class ProductDetailsPage extends StatelessWidget {
             ),
 
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  "\$${product['price']}",
+                  "\$${widget.product['price']}",
                   style: Theme.of(context).textTheme.titleLarge,
                 ),
                 const SizedBox(height: 10),
@@ -45,15 +53,43 @@ class ProductDetailsPage extends StatelessWidget {
                   height: 50,
                   child: ListView.builder(
                     scrollDirection: Axis.horizontal,
-                    itemCount: (product["sizes"] as List<int>).length,
+                    itemCount: (widget.product["sizes"] as List<int>).length,
                     itemBuilder: (context, index) {
-                      final size = (product["sizes"] as List<int>)[index];
+                      final size =
+                          (widget.product["sizes"] as List<int>)[index];
 
                       return Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Chip(label: Text(size.toString())),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedSize = size;
+                            });
+                          },
+                          child: Chip(
+                            backgroundColor: selectedSize == size
+                                ? Theme.of(context).colorScheme.primary
+                                : null,
+                            label: Text(size.toString()),
+                          ),
+                        ),
                       );
                     },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      minimumSize: Size(double.infinity, 50),
+                    ),
+                    label: Text(
+                      "Add to Cart",
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                    icon: Icon(Icons.shopping_cart),
                   ),
                 ),
               ],
---
+++ lib/product_list.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/global_variables.dart';
+import 'package:weather_app/product_card.dart';
+import 'package:weather_app/product_details_page.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({super.key});
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  final List<String> filters = const ["All", "Addidas", "Nike", "Bata"];
+  late String selectedFillter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFillter = filters[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
+    );
+
+    return SafeArea(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  "Shoes\nCollection",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: border,
+                    enabledBorder: border,
+                    focusedBorder: border,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filters.length,
+              itemBuilder: (context, index) {
+                final filter = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFillter = filter;
+                      });
+                    },
+                    child: Chip(
+                      backgroundColor: selectedFillter == filter
+                          ? Theme.of(context).colorScheme.primary
+                          : const Color.fromRGBO(245, 247, 249, 1),
+                      side: BorderSide(
+                        color: const Color.fromRGBO(245, 247, 249, 1),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      label: Text(filter),
+                      labelStyle: TextStyle(fontSize: 16),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return ProductDetailsPage(product: product);
+                        },
+                      ),
+                    );
+                  },
+                  child: ProductCard(
+                    price: product["price"] as double,
+                    title: product["title"] as String,
+                    image: product["imageUrl"] as String,
+                    backgroundColor: index.isEven
+                        ? const Color.fromRGBO(216, 240, 253, 1)
+                        : const Color.fromRGBO(245, 247, 249, 1),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
